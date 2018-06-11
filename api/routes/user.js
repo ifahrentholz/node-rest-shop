97e1aa608e9dfe4196ebde1a3c4ec21acb7468e4
Ingo Fahrentholz <ingof.work@gmail.com>
2018-06-11 22:33:28 +0200

    Generate a JWT

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -40,6 +41,49 @@ router.route("/signup")
             }
           });
         }
+      });
+  });
+
+
+router.route("/login")
+  .post((req, res, next) => {
+    User.find({email: req.body.email})
+      .exec()
+      .then(user => {
+        if(user.length < 1) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+          if(error) {
+            return res.status(401).json({
+              message: "Auth failed"
+            });
+          }
+          if(result) {
+            const token = jwt.sign({
+              email: user[0].email,
+              userId: user[0]._id
+            }, 
+            'secret',
+            {
+              expiresIn: "1h"
+            });
+            return res.status(200).json({
+              message: "Autho successful",
+              token
+            });
+          }
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error
+        });
       });
   });
 
